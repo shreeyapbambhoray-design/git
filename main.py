6a696ed5c5f32d4a779fb89a9eb05ced71168468
Shreeya P Bambhoray <shreeyapbambhoray@gmail.com>
2026-04-08 23:56:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("hello world")
+public class FCFS {
+    public static void main(String[] args) {
+
+        int[] burstTime = {40, 10}; // Task 1 (long), Task 2 (short)
+        int time = 0;
+
+        for (int i = 0; i < burstTime.length; i++) {
+            time += burstTime[i];
+            System.out.println("Process " + i + " finished at " + time);
+        }
+    }
+}
